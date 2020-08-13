bdaefabd54a2115b465a28688d4a327b47cf7c10
孔纬旭 <479828125@qq.com>
2020-08-13 09:45:40 +0800

    完成用户列表功能的开发

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,20 +1,134 @@
 <template>
-    <div>
-        <el-button type="info" @click="logout">退出</el-button>
-    </div>
+  <el-container class="home-container">
+    <!--头部-->
+    <el-header>
+      <div>
+        <span>电商后台管理系统</span>
+      </div>
+      <el-button type="info" @click="logout">退出</el-button>
+    </el-header>
+    <!--页面主体-->
+    <el-container>
+      <!--侧边栏-->
+      <el-aside :width="isCollapse ? '64px':'200px'">
+        <!--折叠展开功能-->
+        <div class="toggle-button" @click="toggleCollapse">|||</div>
+        <!--侧边栏菜单区-->
+        <el-menu background-color="#333744" text-color="#fff" active-text-color="#409EFF" unique-opened :collapse="isCollapse" :collapse-transition="false" router :default-active="activePath">
+          <!--一级菜单，加上:表示动态数据绑定-->
+          <el-submenu :index="item.id + ''"  v-for="item in menuList" :key="item.id">
+            <!--一级菜单的模板区域-->
+            <template slot="title">
+              <!--图标-->
+              <i :class="iconsObj[item.id]"></i>
+              <!--文本-->
+              <span>{{item.authName}}</span>
+            </template>
+            <!--二级菜单-->
+            <el-menu-item :index="'/' + subItem.path" v-for="subItem in item.children" :key="subItem.id" @click="saveNavState('/' + subItem.path)">
+              <template slot="title">
+              <!--图标-->
+              <i class="el-icon-menu"></i>
+              <!--文本-->
+              <span>{{subItem.authName}}</span>
+              </template>
+            </el-menu-item>
+          </el-submenu>
+        </el-menu>
+      </el-aside>
+      <!--右侧内容主体-->
+      <el-main>
+        <!--路由占位符-->
+        <router-view></router-view>
+      </el-main>
+    </el-container>
+  </el-container>
 </template>
 
 <script>
 export default {
+  data () {
+    return {
+      menuList: [],
+      iconsObj: {
+        125: 'el-icon-user-solid',
+        103: 'el-icon-message-solid',
+        101: 'el-icon-s-cooperation',
+        102: 'el-icon-folder-opened',
+        145: 'el-icon-bank-card'
+      },
+      // 是否折叠
+      isCollapse: false,
+      // 被高亮的链接地址
+      activePath: ''
+    }
+  },
+  // 定义一个生命周期函数
+  created () {
+    this.getMenuList()
+    this.activePath = window.sessionStorage.getItem('activePath')
+  },
   methods: {
     logout () {
       window.sessionStorage.clear()
       this.$router.push('/login')
+    },
+    // 获取所有菜单
+    async getMenuList () {
+      const { data: res } = await this.$http.get('menus')
+      if (res.meta.status !== 200) return this.$message.error(res.meta.msg)
+      this.menuList = res.data
+      console.log(res)
+    },
+    // 点击按钮切换菜单折叠与展开
+    toggleCollapse () {
+      this.isCollapse = !this.isCollapse
+    },
+    // 保存链接激活状态
+    saveNavState (activePath) {
+      window.sessionStorage.setItem('activePath', activePath)
+      this.activePath = activePath
     }
   }
 }
 </script>
 
 <style lang="less" scoped>
-
+.el-header {
+  background-color: #373d41;
+  // header一般使用flex布局
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  color: #fff;
+  font-size: 20px;
+  > div {
+    display: flex;
+    align-items: center;
+  }
+}
+.el-aside {
+  background-color: #333744;
+  .el-menu {
+    border-right: none;
+  }
+}
+.el-main {
+  background-color: #eaedf1;
+}
+.home-container {
+  height: 100%;
+}
+.iconfont {
+  margin-right: 10px;
+}
+.toggle-button {
+  background-color: #4A5064;
+  font-size: 10px;
+  line-height: 24px;
+  color: #ffffff;
+  text-align: center;
+  letter-spacing: 0.2em;
+  cursor: pointer;
+}
 </style>
--- src/main.js
+++ src/main.js
@@ -3,8 +3,15 @@ import App from './App.vue'
 import router from './router'
 import './plugins/element.js'
 import axios from 'axios'
+
 // 配置请求的根路径
 axios.defaults.baseURL = 'http://127.0.0.1:8888/api/private/v1/'
+axios.interceptors.request.use(config => {
+  console.log(config)
+  config.headers.Authorization = window.sessionStorage.getItem('token')
+  // 在最后必须return config
+  return config
+})
 Vue.prototype.$http = axios
 
 Vue.config.productionTip = false

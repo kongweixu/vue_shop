02b27e06ac6956ef0d25133837a25df05169fa7a
孔纬旭 <479828125@qq.com>
2020-08-15 09:57:20 +0800

    完成了权限功能开发

--- src/router/index.js
+++ src/router/index.js
@@ -4,6 +4,8 @@ import Login from '../components/Login.vue'
 import Home from '../components/Home.vue'
 import Welcome from '../components/Welcome.vue'
 import Users from '../components/user/Users.vue'
+import Rights from '../components/power/Rights.vue'
+import Roles from '../components/power/Roles.vue'
 
 // 导入全局样式表
 import '../assets/css/global.css'
@@ -12,7 +14,16 @@ Vue.use(VueRouter)
 const routes = [
   { path: '/', redirect: '/login' },
   { path: '/login', component: Login },
-  { path: '/home', component: Home, redirect: '/welcome', children: [{ path: '/welcome', component: Welcome }, { path: '/users', component: Users }] }
+  {
+    path: '/home',
+    component: Home,
+    redirect: '/welcome',
+    children: [
+      { path: '/welcome', component: Welcome },
+      { path: '/users', component: Users },
+      { path: '/rights', component: Rights },
+      { path: '/roles', component: Roles }]
+  }
 ]
 
 const router = new VueRouter({
